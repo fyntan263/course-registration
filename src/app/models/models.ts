b5b24542d4c3de89ff807a1ac8d88326352ee94f
Logan <112201016@smail.iitpkd.ac.in>
2024-06-20 11:01:03 +0530

    new changes in couurse component

--- src/app/models/models.ts
+++ src/app/models/models.ts
@@ -1,71 +1,84 @@
+import { PrerequisiteService } from "../services/prerequisite.service";
+
 // Clause that contains a list of codes, representing AND conditions
 export interface PrerequisiteClause {
-  code: string[];
-}
+    code: string[];
+  }
+  
+  // Interface for course prerequisites with OR and NOT clauses
+  export interface Prerequisite {
+    orClause: PrerequisiteClause[];
+    notClause: string[];
+  }  
+  
+  // Interface representing the course details
+  export interface Course {
+    code: string;
+    name: string;
+    credits: string; // Format: "L-T-P-C"
+    instructor: string[];
+    preRequisites: Prerequisite;
+    coRequisites: Prerequisite;
+    slot: string;
+    textBooks: string[];
+    referenceBooks: string[];
+    syllabusLink: string;
+    programCode:string 
+    targetedBranch:string;
+    isCore: boolean
+    isElective: boolean
+    electiveCategory:string //PME, HSE, 
+    quota:number
+    courseLevel: number;
+  }
+  export interface Range {
+    max: number;
+    min: number;
+  }
 
-// Interface for course prerequisites with OR and NOT clauses
-export interface Prerequisite {
-  orClause: PrerequisiteClause[];
-  notClause: string[];
-}  
-
-// Interface representing the course details
-export interface Course {
-  code: string;
-  name: string;
-  credits: string; // Format: "L-T-P-C"
-  instructor: string[];
-  preRequisites: Prerequisite;
-  coRequisites: Prerequisite;
-  slot: string;
-  textBooks: string[];
-  referenceBooks: string[];
-  syllabusLink: string;
-  programCode:string 
-  targetedBranch:string;
-  isCore: boolean
-  isElective: boolean
-  electiveCategory:string //PME, HSE, 
-  quota:number
-  courseLevel: number;
+  export interface ElectiveRange extends Range {
+    code: string
 }
-export interface Range {
-  max: number;
-  min: number;
-}
+  
+  export interface PrerequisiteWaiver{
+    code: string;
+    status: PrerequisiteWaiverStatus
+  }
+  // Interface extending StudentProfile with additional course-related information
+  export interface StudentInfo{
+    rollNo: string;
+    isEligible: boolean;
+    electiveRange: ElectiveRange[]
+    creditRange: Range;
+    completedCourses: string[]; // List of past courses (including electives)
+    preRequisiteWaivers: PrerequisiteWaiver[];
+    totalCredits: number;
+    electiveCredits: ElectiveCredits[];
 
-export interface ElectiveRange extends Range {
-  code: string
-}
+  }
+ export interface PrerequisiteWaiverRequest{
+  rollNo: string,
+  courseCode: string,
+  reason: string,
+  preReqWaiverRequest: boolean
+ }
 
-// Interface extending StudentProfile with additional course-related information
-export interface StudentInfo{
-  rollNo: string;
-  isEligible: boolean;
-  electiveRange: ElectiveRange[]
-  creditRange: Range;
-  completedCourses: string[]; // List of past courses (including electives)
-  preRequisiteWaivers: PrerequisiteWaiverRequest[];
-  totalCredits: number;
-  electiveCredits: ElectiveCredits[];
-
-}
-export interface PrerequisiteWaiverRequest{
-rollNo: string,
-courseCode: string,
-reason: string,
-preReqWaiverRequest: boolean
-}
-
-export interface CoreCoursePlanSubmission {
-  rollNo: string;
-  riskStatus: boolean;  // Changed to more descriptive naming for properties
-  totalCredits: number;
-  coreCoursePlan: Course[];
+  export interface CoreCoursePlanSubmission {
+    rollNo: string;
+    riskStatus: boolean;  // Changed to more descriptive naming for properties
+    totalCredits: number;
+    coreCoursePlan: Course[];
 }
 
 export interface ElectiveCredits {
-basket: string[];
-minCredits: number;
-maxCredits: number;
+	basket: string[];
+	minCredits: number;
+  maxCredits: number;
+}
+
+export enum PrerequisiteWaiverStatus {
+  APPLIED = "APPLIED",
+  APPROVED = "APPROVED",
+  REJECTED = "REJECTED",
+  NOT_APPLIED = "NOT_APPLIED" 
 }
