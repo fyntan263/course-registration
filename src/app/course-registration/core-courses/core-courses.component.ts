8cbf10eb9a096dd8ecede472834608a981e41525
fyntan263 <samuelfyntan@gmail.com>
2024-06-25 10:42:44 +0530

    -

---
+++ src/app/course-registration/core-courses/core-courses.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-core-courses',
+  standalone: true,
+  imports: [],
+  templateUrl: './core-courses.component.html',
+})
+export class CoreCoursesComponent {
+
+}
---
+++ src/app/course-registration/courses-offered/courses-offered.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courses-offered',
+  standalone: true,
+  imports: [],
+  templateUrl: './courses-offered.component.html'
+})
+export class CoursesOfferedComponent {
+
+}
